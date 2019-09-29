1abe738ca02277838d710fe047ce3f4a69e2758e
sobka <pawel.sobczak0@gmail.com>
2019-09-29 19:31:46 +0200

    Finishing touches

--- mosaic.py
+++ mosaic.py
@@ -7,6 +7,7 @@ import fnmatch
 import operator
 import logging
 import time
+from sys import argv
 
 from timeit import default_timer
 import random
@@ -37,20 +38,12 @@ def assing_color_name(averaged_color):
 
 def average_image(image_to_average):
     """Averages image to single RGB value."""
-    rgb = []
-    if len(image_to_average) > 3:
-        average = np.average(image_to_average, (0, 1))
-        rgb.append(average[2])
-        rgb.append(average[1])
-        rgb.append(average[0])
-    else:
-        rgb = np.average(image_to_average)
-    return rgb
+    return np.average(image_to_average, (0, 1))
 
 
-def create_folders():
+def create_folders(dest_dir):
     """Creates color-coded folders based on XKCD_COLORS color dict."""
-    directory = "colors"
+    directory = os.path.join(dest_dir, 'colors')
     os.makedirs(directory, exist_ok=True)
     for color in XKCD_COLORS:
         if not os.path.exists(color):
@@ -65,20 +58,26 @@ def imgs_close_enough(imageA, imageB, diff):
     return np.allclose(imageA, imageB, atol=diff)
 
 
-def copy_images_to_folders():
-    create_folders()
+def copy_images_to_folders(source_dir, dest_dir):
+    """Sorts images in directory by color"""
+    create_folders(dest_dir)
+
+    folders = glob.glob("colors\*")
+    images = glob.glob(os.path.join(source_dir, '*'))
+
     j = 0
-    for image in IMAGES:
+    for image in images:
 
         img_load = cv.imread(image)
         img = average_image(img_load)
         assigned = assing_color_name(img)
 
-        for folder in glob.glob("colors\*"):
+        for folder in folders:
             folder_name = folder.split('\\')[1]
             image_name = image.split('\\')[1]
             if assigned['name'] == folder_name:
-                dest = os.path.join('colors', folder_name, image_name)
+                dest = os.path.join(dest_dir, 'colors',
+                                    folder_name, image_name)
                 copy(image, dest)
                 print(f"""{j}. Copied {image_name} to {folder_name}""")
                 j += 1
@@ -98,13 +97,16 @@ def remove_duplicates(directory_path):
             print(f'Removing {image} ...')
 
 
-def mosaic_image(image, step):
+def mosaic_image(image, step, output):
+    """Reads image and returns new mosaiced image."""
     i = 0
     img = cv.imread(image)
     width, height = img.shape[:2]
     new_image = np.zeros((width, height, 3), np.uint8)
+    step = int(step)
 
-    if width % step is not 0 or height % step is not 0:
+    # Make so image is divisible by step
+    if width % step != 0 or height % step != 0:
         width = (width // step) * step
         height = (height // step) * step
 
@@ -117,69 +119,58 @@ def mosaic_image(image, step):
             averaged_square = average_image(square)
             square_color = assing_color_name(averaged_square)
 
-            # {'name': 'camouflage green', 'rgb': [71.38, 87.9925, 28.115]} - square_color
             # Get list of color matching images
             possible_images = get_folders_content(
                 os.path.join('colors', square_color['name']))
 
-            # if not possible_images[square_color['name']]:
-            #     continue
-
+            # Get numer of possible color matching images
             number_of_possible_images = len(
                 possible_images[square_color['name']])
+
+            # If there are not matching colors go to next square
             if number_of_possible_images == 0:
-                # TODO: If True add solid color square instead of new image
                 continue
 
+            # Select random color matching image
             random_number = random.randrange(0, number_of_possible_images)
             random_matching_image = possible_images[square_color['name']
                                                     ][random_number]
 
-            # Copy color images into new image
+            # Copy color matching image into new image
             loaded_matching_image = cv.imread(os.path.join(
                 'colors', square_color['name'],  random_matching_image))
             resized_image = cv.resize(loaded_matching_image, (step, step))
-            # cv.imshow('sad', resized_image)
-
-            # cv.waitKey(0)
             new_image[x:x+step, y:y+step] = resized_image
+
+            # Console output
             i += 1
             print(
                 f'Square {i} - {square_color["name"]} --> {random_matching_image}')
-
-    return new_image
-
-    # print(square)
+            cv.imwrite(f'{output}', new_image)
+    print(f'Saved mosaiced image as {output}')
 
 
 def get_folders_content(dir_name):
+    """Reads a directory content and returns dict {folder_name: list_of_images}."""
     contents = {}
     for root, dirs, files in os.walk(dir_name):
         contents[root[7:]] = files
-        # print(root[7:], len(files))
     return contents
 
 
-# print(get_folders_content('colors\\red'))
-
-# a = get_folders_content()
-# sorte = sorted(a.items(), key=lambda k: k[1], reverse=True)
-# for d in sorte:
-#     print(d[0], d[1])
-# # print(sorte)
-# ja = cv.imread('kasia.jpg')
-# print(ja.shape[:2])
+def print_help():
+    print('Mosaic - Help')
+    print('-d [source directory]')
+    print('-s [source directory] [destinatino directory]')
+    print('-m [image source] [step] [image output]')
 
 
-start = default_timer()
-a = mosaic_image('ja.jpg', 5)
-end = default_timer()
-print((end - start) / 60)
-cv.imwrite(f'{str(time.time())[:10]}_mosaiced.jpg', a)
-cv.imshow('asd ', a)
-cv.waitKey(0)
-
-
-# if __name__ == "__main__":
-#     # remove_duplicates('images')
-#     copy_images_to_folders()
+if __name__ == "__main__":
+    if argv[1] == '-m':
+        mosaic_image(argv[2], argv[3], argv[4])
+    elif argv[1] == '-d':
+        remove_duplicates(argv[2])
+    elif argv[1] == '-s':
+        copy_images_to_folders(argv[2], argv[3])
+    elif argv[1] == '-h' or argv[1] == '--help':
+        print_help()
